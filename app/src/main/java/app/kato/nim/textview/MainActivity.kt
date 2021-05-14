ba7443f0569b5c5226d0d627095739dd302092d4
mina1112 <mina.kato8885@gmail.com>
2021-05-15 02:04:37 +0900

    TextViewスキル

---
+++ app/src/main/java/app/kato/nim/textview/MainActivity.kt
@@ -0,0 +1,17 @@
+package app.kato.nim.textview
+
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        textView.text = "Hello Android!"
+
+        textView.setTextColor(Color.rgb(200,50,80))
+}
+}
